38ef119b4bffb60e2479bd1813882627d0d9ebf0
aryan7122 <aryankushwahalifenote@gmail.com>
2025-07-14 14:37:17 +0530

    --

--- src/SALON/Page/NewsArticles/NewsArticles.jsx
+++ src/SALON/Page/NewsArticles/NewsArticles.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./NewsArticles.scss";
-import img1 from '../../../assets/SALON/hero/NewsArticles/img1.png'
-import img2 from '../../../assets/SALON/hero/NewsArticles/img2.png'
-import img3 from '../../../assets/SALON/hero/NewsArticles/img3.png'
+import img1 from "../../../assets/SALON/hero/NewsArticles/img1.png";
+import img2 from "../../../assets/SALON/hero/NewsArticles/img2.png";
+import img3 from "../../../assets/SALON/hero/NewsArticles/img3.png";
 
 const articles = [
   {
@@ -10,39 +10,36 @@ const articles = [
     description:
       "Discover the perfect pre-event skincare routine including facials, hydrating treatments, and clinical therapies to look radiant on your big day.",
     date: "10July",
-    image:img1,
+    image: img1,
   },
   {
     title: "7 Signs You Need a Hair Spa (And What to Expect)",
     date: "10July",
-    image:img2,
+    image: img2,
   },
   {
     title: "Top 5 Salon Trends in Bangalore This Year",
     date: "10July",
-   image:img3,
+    image: img3,
   },
 ];
 
 export default function NewsArticles() {
   return (
     <section className="news-section">
-      <div className="right">
-        <p className="tag">latest</p>
-        <h2>News & Articles</h2>
-        <p className="sub">Lorem ipsum dolor sit amet consectetur. Mattis sapien amet a egestas.</p>
-      </div>
-
-      <div className="left">
-        {articles.map((article, index) => (
-          <div className={`news-card ${index === 0 ? "large" : "small"}`} key={index}>
+      <div className="card">
+        {articles.slice(0, 1).map((article, index) => (
+          <div
+            className={`news-card ${index === 0 ? "large" : "small"}`}
+            key={index}
+          >
             <img src={article.image} alt={article.title} className="news-img" />
             <div className="text">
               <h4>{article.title}</h4>
               {article.description && <p>{article.description}</p>}
               <div className="bottom">
                 <span>{article.date}</span>
-                {index !== 0 && <span className="read">Read more</span>}
+               <span className="read">Read more</span>
               </div>
             </div>
           </div>
@@ -50,10 +47,38 @@ export default function NewsArticles() {
       </div>
 
       <div className="see-all">
+        <div className="right">
+          <p className="tag">latest <br/> News & Articles</p>
+          
+          <p className="sub">
+            Lorem ipsum dolor sit amet consectetur. Mattis sapien amet a
+            egestas.
+          </p>
+        </div>
+       <div className=" card2">
+        {articles.slice(1, 3).map((article, index) => (
+          <div
+            className={`news-card ${index === 0 ? "large" : "small"}`}
+            key={index}
+          >
+            <img src={article.image} alt={article.title} className="news-img" />
+            <div className="text">
+              <h4>{article.title}</h4>
+              {article.description && <p>{article.description}</p>}
+              <div className="bottom">
+                <span>{article.date}</span>
+                <span className="read">Read more</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+       <div>
         <div className="circle">
           <span className="arrow">↗</span>
         </div>
         <p>See All</p>
+       </div>
       </div>
     </section>
   );
