1b66c81f0a760dd71e0e70aa0731b99baeface82
aryan7122 <aryankushwahalifenote@gmail.com>
2025-07-31 10:31:51 +0530

    -|-|-

--- src/util/WebPImage.jsx
+++ src/util/WebPImage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 // import { LazyLoadImage } from "react-lazy-load-image-component";
 // import "react-lazy-load-image-component/src/effects/blur.css";
-import placeholderImg from "../assets/babypng_loader.png";
+import placeholderImg from "../assets/babypng_loader.gif";
 // import placeholderImg from "../assets/LazyLoadImage.png";
 import "./WebPImage.scss";
 const WebPImage = ({ src, alt, className }) => {
